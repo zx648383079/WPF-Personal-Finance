74da3e9b09811779487b483f6599e27fb7ff7954
zx648383079 <648383079@qq.com>
2016-03-05 12:32:05 +0800

    未完成

---
+++ ZoDream/ViewModel/AddViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace ZoDream.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class AddViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the AddViewModel class.
+        /// </summary>
+        public AddViewModel()
+        {
+        }
+    }
+}
